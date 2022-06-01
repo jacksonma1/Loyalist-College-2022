412c1e43c215f0d861d17b87b56b826c81a4a5e4
jacksonma1 <72176419+jacksonma1@users.noreply.github.com>
2022-06-01 01:29:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+window.onscroll = function() {myFunction()};
+
 $(".readmore-link").click( function(e) {
     // record if our text is expanded
     var isExpanded =  $(e.target).hasClass("expand");
@@ -12,7 +14,7 @@ $(".readmore-link").click( function(e) {
       $(e.target).addClass("expand");  
     } 
 });
-  
+
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.defaults({
@@ -53,7 +55,7 @@ gsap.to("#page2line", {
 });
 gsap.to(".two", {
     scrollTrigger:".one",
-    delay:3.5,
+    delay:4,
     duration: 0,
     ease:"none", onComplete: showTwo
 });
@@ -63,43 +65,71 @@ gsap.to("#page2line", {
 });
 gsap.to("#bigcircle", {
     scrollTrigger:".two",
-    y:"745px", x:"25px",
+    y:"635.73px", x:"21.33px",
     duration: 2, ease:"bounce", onComplete: nextcircle
 });
 function nextcircle (){
-    gsap.fromTo("#bigcircle", {y:"725px", x:"25px"}, {
-        scrollTrigger:".two", y:"455px", x:"25px", delay: 0, duration: 1, ease:"elastic"
+    gsap.fromTo("#bigcircle", {y:"618.67px", x:"21.33px"}, {
+        scrollTrigger:".two", y:"395px", x:"21.33px", delay: 0, duration: 1, ease:"elastic"
     })
-    gsap.fromTo("#page2text", {y:"725px", x:"25px"}, {
-        scrollTrigger:".two", y:"440px", x:"25px", delay: 0, duration: 1, ease:"elastic"
+    gsap.fromTo("#page2text", {y:"618.67px", x:"21.33px"}, {
+        scrollTrigger:".two", y:"375.46px", x:"21.33px", delay: 0, duration: 1, ease:"elastic"
     })
 }
 function nextdot (){
-    gsap.fromTo("#page3dot", {y:"1025px"}, {scrollTrigger:".two", y:"1320px", delay:0, duration: 1, ease:"bounce"})
+    gsap.fromTo("#page3dot", {y:"874.67px"}, {scrollTrigger:".two", y:"1126.4px", delay:0, duration: 1, ease:"bounce"})
 }
 function showTwo (){
     $(document).ready(function() {
       $('#panel_two').css('display', 'block');
   });
 }
+function hideOne (){
+    $(document).ready(function() {
+      $('#panel_one').css('display', 'none');
+  });
+}
+function hideTwo (){
+    $(document).ready(function() {
+      $('#panel_two').css('display', 'none');
+  });
+}
+function showNoAnimation (){
+    $(document).ready(function() {
+        $('#no_animation').css('display', 'block');
+    });
+}
+function hideNoAnimation (){
+    $(document).ready(function() {
+        $('#no_animation').css('display', 'none');
+    });
+}
 function hideAnimation (){
-  $(document).ready(function() {
-    $('#animation').css('display', 'none');
-});
+    $(document).ready(function() {
+        $('#animation').css('display', 'none');
+    });
+    hideNoAnimation ();
+}
+function closeAnimation (){
+    hideOne();
+    hideTwo();
+    $(document).ready(function() {
+        $('.containa').css('overflow', 'hidden');
+    });
 }
 gsap.to("#page2text", {
     scrollTrigger:".two",
-    y:"725px", x:"25px",
+    y:"618.67px", x:"21.33px",
     duration: 2, ease:"bounce"
 });
 gsap.to("#page3dot", {
     scrollTrigger:".two",
-    y:"1025px",
+    y:"874.67px",
     duration: 2, delay: 0, ease:"none", onComplete:nextdot
 });
 gsap.to("#page3text", {
     scrollTrigger:".two",
-    y:"850px", x:"200px",
+    y:"725.33px", x:"170.66px",
     duration: 3, ease:"none"
 });
 gsap.to("#page3text", {
@@ -112,24 +142,20 @@ gsap.to("#seasaw", {
     rotation:0, duration: 1, delay: 2, ease:"elastic"
 })
 gsap.from("#page5text", {
-    scrollTrigger:".two", opacity: 0, delay: 4, duration: 1, ease:"none"
+    scrollTrigger:".two", opacity: 0, delay: 3, duration: 1, ease:"none"
 })
 gsap.to("#page3dot", {
     scrollTrigger:".two",
-    scale:25, delay: 6.5, duration: 1, ease:"none"
+    scale:25, delay: 5, duration: 1, ease:"none"
 })
 gsap.to("#logo2x2", {
     scrollTrigger:".two",
-    opacity:1, delay: 7.5, duration: 1
+    opacity:1, delay: 6, duration: 1
 })
 gsap.to("#title2x2", {
     scrollTrigger:".two",
-    opacity:1, delay: 7.5, duration: 1
+    opacity:1, delay: 6, duration: 1
 })
 gsap.to("#animation", {
-  scrollTrigger:".two",
-  opacity:0, delay: 10, duration: 1
-})
-gsap.to("#animation", {
-  scrollTrigger:".two", delay: 12, duration: 0, onComplete: hideAnimation
+  scrollTrigger:".two", delay: 7, duration: 0, onComplete: closeAnimation
 })
